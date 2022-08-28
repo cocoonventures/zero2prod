5f337ae5453e0a8276ed27b213b5801e69d13d0b
Michael Madison <chiefhacker@cocoonventures.com>
2022-08-27 21:05:36 -0500

    formated

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 #![allow(unused_imports)]
-use actix_web::{web, App, HttpRequest, HttpServer, Responder, HttpResponse};
+use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
 
 // async fn greet(req: HttpRequest) -> impl Responder {
 //     let name = req.match_info().get("name").unwrap_or("World");
@@ -13,10 +13,9 @@ async fn health_check() -> impl Responder {
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
-        App::new()
-            .route("/health_check",web::get().to(health_check))
-            // .route("/", web::get().to(greet))
-            // .route("/{name}", web::get().to(greet))
+        App::new().route("/health_check", web::get().to(health_check))
+        // .route("/", web::get().to(greet))
+        // .route("/{name}", web::get().to(greet))
     })
     .bind("127.0.0.1:8000")?
     .run()
