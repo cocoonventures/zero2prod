2cc0e530e0823510a7b794695e4d0680c181e597
Michael Madison <chiefhacker@cocoonventures.com>
2022-09-01 02:44:24 -0500

    broken test, around pg 85

--- src/lib.rs
+++ src/lib.rs
@@ -7,8 +7,15 @@ async fn health_check() -> impl Responder {
     HttpResponse::Ok().finish()
 }
 
+async fn subscriptions() -> impl Responder{
+    HttpResponse::Ok().finish()
+}
+
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
+    let server = HttpServer::new(|| App::new().
+            route("/health_check", web::get().to(health_check))
+        .route("/subscriptions",web::post().to(subscriptions))
+        )
         .listen(listener)?
         .run();
     Ok(server)
