e3ab9685b92aa68d686daa6a0406992c1aeeca15
Michael Madison <chiefhacker@cocoonventures.com>
2022-09-16 02:41:48 -0500

    cargo check passes

--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,25 @@
 use std::env;
 use std::net::TcpListener;
 
-use migration::{Migrator, MigratorTrait};
+use sea_orm::Database;
 use sea_orm::DatabaseConnection;
+use sea_orm::error::DbErr;
+
 // use sea_orm::{entity::*, query::*};
 // use serde::{Deserialize, Serialize};
-
+use migration::{Migrator, MigratorTrait};
 use zero2prod::run;
 
-#[tokio::main]
-async fn main() -> std::io::Result<()> {
-    let db_url = env::var("ZERO2PROD_DB_URL")?;
+async fn get_db() -> Result<DatabaseConnection, DbErr> {
+    let db_url = env::var("ZERO2PROD_DB_URL").expect("ZERO2PROD_DB must be defined");
     let db: DatabaseConnection = Database::connect(db_url).await?;
+    Ok(db)
+}
 
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+
+    let db = get_db().await.expect("Problem getting db connection");
 
     let listener = TcpListener::bind("127.0.0.1:8000")?;
     run(listener)?.await
