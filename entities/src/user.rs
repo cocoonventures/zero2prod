8697c05db38cbb4ddd37478ec568000946ea197a
Michael Madison <chiefhacker@cocoonventures.com>
2022-09-23 17:49:19 -0500

    OMG compiles, should insert untested

--- entities/src/user.rs
+++ entities/src/user.rs
@@ -2,7 +2,7 @@
 
 use sea_orm::entity::prelude::*;
 
-#[derive(Clone, Debug, PartialEq, DeriveEntityModel)]
+#[derive(Clone, Debug, PartialEq, DeriveEntityModel)] //, Serialize, Deserialize)]
 #[sea_orm(table_name = "user")]
 pub struct Model {
     #[sea_orm(primary_key)]
--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,5 +1,11 @@
 #[allow(unused)]
 use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
+use chrono::prelude::*;
+use chrono::Utc;
+use entities::*;
+use sea_orm::ActiveModelTrait;
+use sea_orm::ActiveValue::*;
+use sea_orm::DatabaseConnection;
 
 #[allow(unused)]
 #[derive(serde::Deserialize)]
@@ -8,6 +14,29 @@ pub struct FormData {
     name: String,
 }
 
-pub async fn subscriptions(_form: web::Form<FormData>) -> impl Responder {
+pub async fn subscriptions(
+    _form: web::Form<FormData>,
+    _db: web::Data<DatabaseConnection>,
+) -> impl Responder {
+    let user = user::ActiveModel {
+        name: Set(_form.name.to_owned()),
+        email: Set(_form.email.to_owned()),
+        ..Default::default()
+    };
+    let user = user
+        .insert(_db.get_ref())
+        .await
+        .expect("Problem inserting user into db");
+
+    let subscription = subscription::ActiveModel {
+        user_id: sea_orm::ActiveValue::Set(user.id),
+        subscribed_at: Set(Utc::now().with_timezone(&FixedOffset::east(0))),
+        ..Default::default()
+    };
+    let subscription = subscription
+        .insert(_db.get_ref())
+        .await
+        .expect("Problem inserting subscription for userinto db");
+
     HttpResponse::Ok().finish()
 }
