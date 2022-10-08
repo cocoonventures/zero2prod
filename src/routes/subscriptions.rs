add6e1c8674a0a0cc4acbcf7cd6758f5a014ef97
Michael Madison <chiefhacker@cocoonventures.com>
2022-10-07 22:12:16 -0500

    adding tracing subscriber

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -3,12 +3,10 @@ use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
 use chrono::prelude::*;
 use chrono::Utc;
 use entities::*;
-// use log::{error, info};
 use sea_orm::ActiveValue::*;
 use sea_orm::{ActiveModelTrait, ConnectOptions};
 use sea_orm::{Database, DatabaseConnection};
-use std::ops::Deref;
-use tracing::*;
+use tracing::*; // Instrument; // use log::{error, info};
 use uuid::Uuid;
 
 #[allow(unused)]
@@ -23,10 +21,12 @@ pub async fn subscribe(
     db_pool: web::Data<ConnectOptions>,
 ) -> impl Responder {
     let request_id = Uuid::new_v4();
-    info!(
-        "[r-uuid:{}] Adding user[\"{}\" ({})] as a new subscriber",
-        request_id, form.name, form.email
+    let request_span = info_span!(
+        "Adding new subscriber",
+        %request_id, subscriber_name = %form.name, subscriber_email = %form.email
     );
+    let _request_span_guard = request_span.enter();
+
     let opts = db_pool.get_ref().clone();
     let db = Database::connect(opts).await.expect(
         format!(
@@ -40,33 +40,35 @@ pub async fn subscribe(
         email: Set(form.email.to_owned()),
         ..Default::default()
     };
-    info!(
-        "[r-uuid:{}] Adding user[\"{}\" {}]",
-        request_id, form.name, form.email
+    let user_span = tracing::info_span!(
+        "Adding user to database",
+        %request_id, subscriber_name = %form.name, subscriber_email = %form.email
     );
     let user = user
         .insert(&db)
+        .instrument(user_span)
         .await
         .expect(format!("[r-uuid:{}] Problem inserting user into db", request_id).as_str());
 
-    info!("[r-uuid:{}] Adding user's subscription", request_id);
+    let sub_span = tracing::info_span!("Adding user's associated subscription", %request_id);
     let subscription = subscription::ActiveModel {
         user_id: Set(user.id),
         subscribed_at: Set(Utc::now().with_timezone(&FixedOffset::east(0))),
         ..Default::default()
     };
-    match subscription.insert(&db).await {
+    match subscription.insert(&db).instrument(sub_span).await {
         Ok(_) => {
-            info!(
+            tracing::info!(
                 "[r-uuid:{}] User subscription saved successfully",
                 request_id
             );
             HttpResponse::Ok().finish()
         }
         Err(e) => {
-            error!(
+            tracing::error!(
                 "[r-uuid:{}] Failed to save subscription, {:?}",
-                request_id, e
+                request_id,
+                e
             );
             HttpResponse::InternalServerError().finish()
         }
